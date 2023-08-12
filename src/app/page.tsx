7a29534c55938d66ee432d0e50adc18ef087a464
Hossein Shayesteh <hosseinxshayesteh@gmail.com>
2023-08-12 19:06:12 +0330

    Add Feature, Pricing and  Newsletter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,18 @@ import React from "react";
 
 import MegaHeader from "@/components/layout/home/MegaHeader";
 import Navbar from "@/components/layout/home/Navbar";
+import Feature from "@/components/layout/home/Feature";
+import Pricing from "@/components/layout/home/Pricing";
+import Newsletter from "@/components/layout/home/Newsletter";
 
 const Home = () => {
   return (
     <>
       <Navbar />
       <MegaHeader />
+      <Feature />
+      <Pricing />
+      <Newsletter />
     </>
   );
 };
