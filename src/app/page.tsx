d68841077497c150fd5fc5f47bb8833efd56b4c3
Hossein Shayesteh <hosseinxshayesteh@gmail.com>
2023-08-12 18:55:44 +0330

    Add navbar and header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
 import React from "react";
-import Link from "next/link";
+
+import MegaHeader from "@/components/layout/home/MegaHeader";
+import Navbar from "@/components/layout/home/Navbar";
 
 const Home = () => {
   return (
-    <div className="flex justify-center mx-auto my-40 text-center text-5xl flex-col text-gray-800">
-      <h1>home page</h1>
-      <Link href={"/auth/register"} className={" mt-5 text-base text-gray-800"}>
-        register
-      </Link>
-    </div>
+    <>
+      <Navbar />
+      <MegaHeader />
+    </>
   );
 };
 
